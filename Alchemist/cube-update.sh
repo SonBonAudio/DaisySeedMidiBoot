c39cd56bbbe39d7c927462fb0a781a983da039f0
michael <michael@cantux.com>
2024-07-08 17:07:39 -0400

    Starting to remove non-bootloader files

--- Alchemist/cube-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# git checkout Src/usbd_desc.c 
-git checkout Makefile
-git checkout Src/usb_device.c 
-# git checkout Inc/FreeRTOSConfig.h Middlewares/Third_Party/FreeRTOS
-# git checkout Src/usb_host.c Inc/usb_host.h 
-rm Src/usbd_audio_if.c
-rm Inc/usbd_audio_if.h
